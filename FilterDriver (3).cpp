e0077312028c408c59cab9e54a1afef3e4fc3abc
ccollins25 <56311983+ccollins25@users.noreply.github.com>
2019-11-05 09:27:08 -0500

    Update FilterDriver (3).cpp

--- FilterDriver (3).cpp
+++ FilterDriver (3).cpp
@@ -26,11 +26,7 @@ char T;
 
 
 ////Function
-void swap(int, int, vector<BaseballStatistic>&);
-void sort(vector<BaseballStatistic>&);
-int search(vector<BaseballStatistic>, string, string);
 void loadFile(vector<BaseballStatistic>&);
-void printHeading();
 
 
 int main(){
@@ -56,7 +52,7 @@ void loadFile (vector<BaseballStatistic>& baseList){
   in.open("BStats.csv");// open file
  
   string temp; // temp variable to hold the old value during the swap
-}
+
   while(getline(in,temp)) // reading file
   {
       string Tname, fname, lname, p;
@@ -68,66 +64,9 @@ void loadFile (vector<BaseballStatistic>& baseList){
        {
 		BaseballStatistic temp(Tname, fname, Jnum, lname, y, m, d, B, T, atbat, avgbat, hr, rbi, sb, ops, era, p);
 		baseList.push_back(temp);
-	   }
-  }
-    void swap(vector<BaseballStatistic>& baseList, int oIdx, int nIdx){
+       }
+   }
     
-    BaseballStatistic temp = baseList[oIdx]; //temporary employee variable to hold the old value during swap
-    baseListList[oIdx] = baseListList[nIdx]; // assign the min_idx(element to be moved) to it's position
-    baseListList[nIdx] = temp; //assign the element previously at the now sorted postion
-    }
-
-    void sort(vector<BaseballStatistic>& baseList){
-    
-     int i, j, min_idx; 
-  
-    // One by one move boundary of unsorted subarray 
-    for (i = 0; i < baseList.size()-1; i++) 
-    { 
-        // Find the minimum element in unsorted array 
-        min_idx = i; 
-      
-        
-        for (j = i+1; j < baseList.size(); j++) 
-          if (
-              (baseList[j].getLName().compare(baseList[min_idx].getLName()) < 0) //if getLName() @ j is < getLName() @ min_idx
-              
-            min_idx = j; 
-      
-        // Swap the found minimum element with the first element 
-        swap(baseList, min_idx, i);
-        
-       
-    } 
-}
-
-
-int search(vector<BaseballStatistic> players, string fname, string lname, string p, string ){
-  
-  int lo = 0;
-  int hi = players.size();
-  
-   while (lo <= hi) 
-    { 
-        int location = lo + (hi/2); 
-  
-        // Check if name is present at mid 
-        if (players[location].getLName() == lname && players[location].getFName() == fname) 
-            return location; 
-  
-        // If name greater, ignore left half 
-        if (players[location].getLName() < lname 
-			|| (players[location].getLName() == lname && players[location].getFName() < fname)) 
-            lo = location + 1; 
-  
-        // If name is smaller, ignore right half 
-        else
-            hi = location - 1; 
-    } 
-  
-    // then element was not present 
-    return -1; 
- 
 }
     
     
