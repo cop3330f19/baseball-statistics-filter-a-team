c8afd6f6edcf644f4e46656c6a61d3a8dbe18551
ccollins25 <56311983+ccollins25@users.noreply.github.com>
2019-11-05 09:36:41 -0500

    Update FilterDriver (3).cpp

--- FilterDriver (3).cpp
+++ FilterDriver (3).cpp
@@ -10,42 +10,47 @@
 
 using namespace std;
 
-int Jnum = 0;
-int atBat = 0;
-int hr = 0;
-int rbi = 0; 
-int sb = 0;
-int d = 0;
-int m = 0;
-int y =0;
-double avgbat = 0;
-double ops = 0; 
-double era = 0;
-char B; 
-char T;
-
-
 ////Function
 void loadFile(vector<BaseballStatistic>&);
 
 
 int main(){
     
+   
+   int Jnum = 0;
+   int atBat = 0;
+   int hr = 0;
+   int rbi = 0; 
+   int sb = 0;
+   int d = 0;
+   int m = 0;
+   int y =0;
+   double avgbat = 0;
+   double ops = 0; 
+   double era = 0;
+   char B; 
+   char T;
+   
     vector<BaseballStatistic> baseList;
 	
-	loadFile(baseList);
-    
-    cout << " Pre Sorted List" << endl;
-    printHeading();
-    for(int )
+    loadFile(baseList);
         
+    do{
+	cout << "Do you want to search for someone (y or yes/n or no)? ";
+	cin >> answer;
+        
+	 cout << "How would you like to find them Team (Position,Batting,Batting Average,Home Runs,Runs Batted In,Stolen Bases,OPS,ERA)? ";
+	 
+    }while(answer != n || answer != no);
     sort(baseList);
     cout << endl << "Sorted " << endl;
     printHeading();
     
     cout << endl << endl;
     string cont ="";
-    
+	
+return 0;
+}   
     
 void loadFile (vector<BaseballStatistic>& baseList){
   ifstream in;
