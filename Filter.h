3e7047893a7cc0a0f55aaad0aa5e83af77abd6e1
tmcmillan1014 <56973171+tmcmillan1014@users.noreply.github.com>
2019-11-13 17:32:07 -0500

    Update Filter.h

--- Filter.h
+++ Filter.h
@@ -1,9 +1,10 @@
 #ifndef FILTER_H
 #define FILTER_H
-
-#include "BaseballStatistic.h"
 #include <string>
-#include <vector> 
+#include <vector>
+#include <iostream>
+#include "StringHelper.h"
+#include "BaseballStatistic.h"
 
 using namespace std;
 
@@ -12,7 +13,7 @@ class Filter {
   static void swap(vector<BaseballStatistic>&, int, int);
   static void namesort(vector<BaseballStatistic>&);
   static void teamsort(vector<BaseballStatistic>&);
-  static int search(vector<BaseballStatistic>&,std::string);
+  static int search(vector<BaseballStatistic>&, std::string);
   static int binarysearch(vector<BaseballStatistic>&, std::string);
 };
 #endif
